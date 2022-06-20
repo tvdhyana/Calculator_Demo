e578e384de8d448b629b15df63a2160e441183a7
tvdhyana <dhyana@techvoot.com>
2022-06-20 17:06:09 +0530

    Calculator demo

--- CalculatorDemo/CalculatorLogic.js
+++ CalculatorDemo/CalculatorLogic.js
@@ -1,22 +1,35 @@
+function display(val) {
+  //Create a list of all operators of the calculator
+  let operators = [...document.querySelectorAll(".operator")].map(
+    (el) => el.textContent
+  );
+
+  //Get the current value
+  let resultElement = document.querySelector("#result");
+  let currentResult = resultElement.value;
+
+  //When the input is an operator and the last character of the result is an operator then overwrite it, otherwise append it.
+  if (
+    operators.includes(val) &&
+    operators.includes(currentResult.charAt(currentResult.length - 1))
+  ) {
+    resultElement.value =
+      currentResult.substring(0, currentResult.length - 1) + val;
+  } else {
+    resultElement.value += val;
+  }
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value = y;
+}
+
+function clr() {
+  document.getElementById("result").value = "";
+}
+
 let screen = document.getElementById("screen");
 buttons = document.querySelectorAll("button");
 let screenValue = "";
-for (item of buttons) {
-  item.addEventListener("click", (e) => {
-    buttonText = e.target.innerText;
-    console.log("Button text is ", buttonText);
-    if (buttonText == "X") {
-      buttonText = "*";
-      screenValue += buttonText;
-      screen.value = screenValue;
-    } else if (buttonText == "C") {
-      screenValue = "";
-      screen.value = screenValue;
-    } else if (buttonText == "=") {
-      screen.value = eval(screenValue);
-    } else {
-      screenValue += buttonText;
-      screen.value = screenValue;
-    }
-  });
-}
